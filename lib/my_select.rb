6c6769edfa23481b1e1bda45e329bc341f641e93
Shawn Koski <shawnnkoski@gmail.com>
2018-04-01 19:37:32 +0000

    Write my own select method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ new_array = []
+ i = 0
+ 
+ while i < collection.size
+   value = yield collection[i]
+   if value == true
+     new_array << collection[i]
+   end
+   i += 1
+ end
+ new_array
 end
